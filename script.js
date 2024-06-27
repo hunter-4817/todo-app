af8ab627a8bc36073eabbdb9cf9697105e4a0f71
hunter-4817 <kishandhandhukiya8@gmail.com>
2024-06-27 22:16:57 +0530

    fix-the-console-error

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+function addTask(){
+    
     const inputBox = document.getElementById("input-box");
     const listContainer = document.getElementById("list-container");
 
-    function addTask(){
-        if(inputBox && inputBox.value ===''){
-            alert("You must write something!");
-        }
-        else if(inputBox && inputBox.value !== ''){
-            let li = document.createElement("li");
-            li.innerHTML = inputBox.value;
-            listContainer.appendChild(li);
-            let span = document.createElement("span");
-            span.innerHTML = "\u00d7";
-            li.appendChild(span);
-        }
-        if(inputBox)
-            inputBox.value = "";
+    if(inputBox && inputBox.value ===''){
+        alert("You must write something!");
     }
-});
+    else if(inputBox && inputBox.value !== ''){
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    if(inputBox)
+        inputBox.value = "";
+}
